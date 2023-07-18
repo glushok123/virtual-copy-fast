f4f45efe60c58766f056c35269fe9f8861506421
Andrey <glushok19999@gmail.com>
2023-07-18 15:24:20 +0300

    шрифт

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,8 +72,22 @@
 	@yield('content')
 
 
-</body>
-</html>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(66824542, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/66824542" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 <style>
 	body{
@@ -85,3 +99,7 @@
 		font-weight: 900 !important;
 	}
 </style>
+
+</body>
+</html>
+
